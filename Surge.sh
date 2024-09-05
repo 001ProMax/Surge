a30e3e6b06e8df9f0414d7559c83e2b983055af7
001ProMax <101964350+001ProMax@users.noreply.github.com>
2024-09-05 18:07:46 +0800

    Update Surge.sh

--- Surge.sh
+++ Surge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 file_path="$1"
 file_dir=$(dirname "$file_path")
-if [[ "$file_dir" == "/Users/001/Library/Mobile Documents/iCloud~com~nssurge~inc/Documents/Script" ]]; then
+if [[ "$file_dir" == "Path/To/Surge/Script" ]]; then
     /Applications/Surge.app/Contents/Applications/surge-cli script evaluate "$file_path" cron 5
 else
     node "$file_path"
