0ffbfa8338597b5478e9404c7a06ea679a164b51
001ProMax <101964350+001ProMax@users.noreply.github.com>
2024-09-11 20:36:20 +0800

    Delete Surge.sh

--- Surge.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-file_path="$1"
-file_dir=$(dirname "$file_path")
-if [[ "$file_dir" == "Path/To/Surge/Script" ]]; then
-    /Applications/Surge.app/Contents/Applications/surge-cli script evaluate "$file_path" cron 5
-else
-    node "$file_path"
-fi
