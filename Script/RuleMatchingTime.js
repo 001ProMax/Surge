ec7f6491095b3214428a4f3abe57a5950e9e588b
001ProMax <101964350+001ProMax@users.noreply.github.com>
2025-07-02 16:52:39 +0800

    Update RuleMatchingTime.js

--- Script/RuleMatchingTime.js
+++ Script/RuleMatchingTime.js
@@ -1,34 +1,29 @@
 let initial = {
-	numberOfRequests: 0,
-	totalMatchingTime: 0,
-	latestRecordTime: 0,
+    numberOfRequests: 0,
+    totalMatchingTime: 0,
+    latestRecordTime: 0,
 };
-let history = JSON.parse(
-	$persistentStore.read("RULEMATCHINGTIME") || JSON.stringify(initial)
-);
+let history = JSON.parse($persistentStore.read("RULEMATCHINGTIME") || JSON.stringify(initial));
 
 $httpAPI("GET", "/v1/requests/recent", null, (body) => {
-	body.requests.forEach((request) => {
-		if (
-			request.timingRecords &&
-			request.setupCompletedDate > history.latestRecordTime &&
-			request.timingRecords[0].name == "Rule Evaluating"
-		) {
-			history.totalMatchingTime += request.timingRecords[0].duration;
-			history.numberOfRequests += 1;
-		}
-	});
+    body.requests.forEach((request) => {
+        if (
+            request.timingRecords &&
+            request.setupCompletedDate > history.latestRecordTime &&
+            request.timingRecords[0].name == "Rule Evaluating"
+        ) {
+            history.totalMatchingTime += request.timingRecords[0].duration;
+            history.numberOfRequests += 1;
+        }
+    });
 
-		history.latestRecordTime = body.requests[0].setupCompletedDate;
-	$persistentStore.write(JSON.stringify(history), "RULEMATCHINGTIME");
+    history.latestRecordTime = body.requests[0].setupCompletedDate;
+    $persistentStore.write(JSON.stringify(history), "RULEMATCHINGTIME");
 
-	let avgMatchingTime = (
-		(history.totalMatchingTime * 1000) /
-		history.numberOfRequests
-	).toFixed($argument);
-	$done({
-		title: "Rule Matching Time",
-		content: `统计数量: ${history.numberOfRequests}\n平均时间: ${avgMatchingTime} ms`,
-		icon: "bolt.horizontal.circle.fill",
-	});
+    let avgMatchingTime = ((history.totalMatchingTime * 1000) / history.numberOfRequests).toFixed($argument);
+    $done({
+        title: "Rule Matching Time",
+        content: `统计数量: ${history.numberOfRequests}\n平均时间: ${avgMatchingTime} ms`,
+        icon: "bolt.horizontal.circle.fill",
+    });
 });
